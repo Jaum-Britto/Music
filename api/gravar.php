1790237964a4a3b52f32b9fbbe8e5c1293567b39
João Gabriel Pfaff de Britto <82790919+Jaum-Britto@users.noreply.github.com>
2024-10-05 17:45:59 -0300

    Update gravar.php

--- api/gravar.php
+++ api/gravar.php
@@ -2,7 +2,7 @@
         $nome = $_POST['nome'];
         $artista = $_POST['artista'];
         $genero =$_POST['genero'];
-        $conexao =    mysqli_connect('35.238.202.154','JP','usbw');
+        $conexao =    mysqli_connect('34.42.216.142','JP','usbw');
                 mysqli_select_db($conexao,'musicas');
         $sql    =    "insert    into    musicas    (Nome,    Artista,    Genero)    values    ('$nome','$artista','$genero')";
     $resposta    =    mysqli_query($conexao, $sql)    or die    ("Erro:    "    .    mysqli_error($conexao
