8f43b33371ce41b5a0d348b20f156e000f3d501e
João Gabriel Pfaff de Britto <82790919+Jaum-Britto@users.noreply.github.com>
2024-10-05 17:45:40 -0300

    Update excluir.php

--- api/excluir.php
+++ api/excluir.php
@@ -1,6 +1,6 @@
 <?php
             $nome=$_POST['nome'];
-            $conexao =    mysqli_connect('35.238.202.154','JP','usbw');
+            $conexao =    mysqli_connect('34.42.216.142','JP','usbw');
                 mysqli_select_db($conexao,'musicas');
             $sql    =    "delete    from    musicas    where    Nome    like    '$nome'    ";
             $resultado    =    mysqli_query($conexao, $sql)    or die    ("Erro:    "    .    mysqli_error($conexao));
