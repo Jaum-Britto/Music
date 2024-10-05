8a19fa8777ebe5a5b7f6ef67b904db03643adfad
João Gabriel Pfaff de Britto <82790919+Jaum-Britto@users.noreply.github.com>
2024-10-05 17:46:30 -0300

    Update selecionar.php

--- api/selecionar.php
+++ api/selecionar.php
@@ -1,6 +1,6 @@
 <?php
                 $nome=$_POST['nome'];
-                $conexao =    mysqli_connect('35.238.202.154','JP','usbw');
+                $conexao =    mysqli_connect('34.42.216.142','JP','usbw');
                 mysqli_select_db($conexao,'musicas');
                 $sql=" select    * from
         musicas    where    Nome    like    '$nome'";
