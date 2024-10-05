a7c00ddba10bf469f03df36494b66ac3f99c3b2c
João Gabriel Pfaff de Britto <82790919+Jaum-Britto@users.noreply.github.com>
2024-10-05 17:45:23 -0300

    Update alterar.php

--- api/alterar.php
+++ api/alterar.php
@@ -3,7 +3,7 @@
                 $nomevelho=$_POST['nomevelho'];
                 $Artista=$_POST['artista'];
                 $genero=$_POST['genero'];
-                $conexao =    mysqli_connect('35.238.202.154','JP','usbw');
+                $conexao =    mysqli_connect('34.42.216.142','JP','usbw');
                 mysqli_select_db($conexao,'musicas');
             $sql    =    "update    musicas    set    Nome='$nomenovo',    Artista='$artista',    Genero='$genero'    where    Nome='$nomevelho'";
             $resultado    =    mysqli_query($conexao, $sql)    or die    ("Erro    .:"    .    mysqli_error($conexao));
